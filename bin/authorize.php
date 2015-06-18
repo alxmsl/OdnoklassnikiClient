778fcfe26f1248e1cf49d8cce8c9561df0616f59
Alexey Maslov <alexey.y.maslov@gmail.com>
2015-06-18 23:00:24 +0300

    Added authorize, token refresh and methods call scripts

---
+++ bin/authorize.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
+ * Script for Odnoklassniki API authorization
+ * @author alxmsl
+ */
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use alxmsl\Cli\CommandPosix;
+use alxmsl\Cli\Exception\RequiredOptionException;
+use alxmsl\Cli\Option;
+use alxmsl\Odnoklassniki\OAuth\Client;
+use alxmsl\Odnoklassniki\OAuth\Response\Token;
+
+$clientId     = '';
+$clientSecret = '';
+$code         = '';
+$redirectUri  = '';
+$scopes       = [];
+
+$Command = new CommandPosix();
+$Command->appendHelpParameter('show help');
+$Command->appendParameter(new Option('code', 'o', 'authorization code', Option::TYPE_STRING)
+    , function($name, $value) use (&$code) {
+        $code = $value;
+    });
+$Command->appendParameter(new Option('client', 'c', 'client id', Option::TYPE_STRING, true)
+    , function($name, $value) use (&$clientId) {
+        $clientId = $value;
+    });
+$Command->appendParameter(new Option('redirect', 'r', 'redirect uri', Option::TYPE_STRING)
+    , function($name, $value) use (&$redirectUri) {
+        $redirectUri = $value;
+    });
+$Command->appendParameter(new Option('scopes', 's', 'grant scopes', Option::TYPE_STRING)
+    , function($name, $value) use (&$scopes) {
+        $scopes = explode(',', $value);
+    });
+$Command->appendParameter(new Option('secret', 'e', 'client secret', Option::TYPE_STRING, true)
+    , function($name, $value) use (&$clientSecret) {
+        $clientSecret = $value;
+    });
+
+try {
+    $Command->parse(true);
+
+    $Client = new Client();
+    $Client->setClientId($clientId)
+        ->setClientSecret($clientSecret)
+        ->setRedirectUri($redirectUri);
+
+    if (!empty($code)) {
+        // Get authorization token for access application
+        $Token = $Client->authorize($code);
+        if ($Token instanceof Token) {
+            printf("%s\n", (string) $Token);
+        } else {
+            var_dump($Token);
+        }
+    } else {
+        // Get authorization uri
+        $uri = $Client->createAuthUrl($scopes, true);
+        printf("%s\n", $uri);
+    }
+} catch (RequiredOptionException $Ex) {
+    $Command->displayHelp();
+}
--- source/OAuth/Response/Token.php
+++ source/OAuth/Response/Token.php
@@ -21,7 +21,7 @@ final class Token implements ObjectInitializedInterface {
     /**
      * Token types constants
      */
-    const TYPE_NONE = '',
+    const TYPE_NONE    = '',
           TYPE_SESSION = 'session';
 
     /**
@@ -131,5 +131,17 @@ final class Token implements ObjectInitializedInterface {
         $this->setAccessToken($Token->getAccessToken())
             ->setTokenType($Token->getTokenType());
         return $this;
+    }
+
+    public function __toString() {
+        $format = <<<'EOD'
+    access token:  %s
+    refresh token: %s
+    token type:    %s
+EOD;
+        return sprintf($format
+            , $this->getAccessToken()
+            , $this->getRefreshToken()
+            , $this->getTokenType());
     }
 }
