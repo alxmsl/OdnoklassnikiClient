9ba43f3764ef1e048d2fa8418f25b26f7a253371
Alexey Maslov <alexey.y.maslov@gmail.com>
2015-06-25 23:05:38 +0300

    WTFPL -> ALv2

--- examples/api.users.getCurrentUser.php
+++ examples/api.users.getCurrentUser.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://www.wtfpl.net/ for more details.
+ * Copyright 2015 Alexey Maslov <alexey.y.maslov@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Odnoklassniki API method users.getCurrentUser example
  * @author alxmsl
