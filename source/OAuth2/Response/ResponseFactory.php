fa24f87dedab0ce6220df08fd58fe0be889d6722
Alexey Maslov <alexey.y.maslov@gmail.com>
2015-08-17 11:14:51 +0300

    up complexity for OAuth response.

--- source/OAuth2/Response/ResponseFactory.php
+++ source/OAuth2/Response/ResponseFactory.php
@@ -18,6 +18,7 @@
 namespace alxmsl\Odnoklassniki\OAuth2\Response;
 
 use RuntimeException;
+use stdClass;
 
 /**
  * Odnoklassniki OAuth server responses factory
@@ -29,28 +30,46 @@ final class ResponseFactory {
      * Create OK OAuth response instance
      * @param string $string response data
      * @return Code|Token|Error response instance
-     * @throws RuntimeException when response has a suspect string
      */
     public static function createResponse($string) {
         $Value = json_decode($string);
         if (json_last_error() === JSON_ERROR_NONE) {
-            switch (true) {
-                case isset($Value->error):
-                    return Error::initializeByObject($Value);
-                default:
-                    return Token::initializeByObject($Value);
-            }
+            return self::createResponseFromJson($Value);
         } else {
-            $value = parse_url($string, PHP_URL_QUERY);
-            if ($value !== false) {
-                switch (true) {
-                    case strpos($value, 'error=') === 0:
-                        return Error::initializeByString($value);
-                    case strpos($value, 'code=') === 0:
-                        return Code::initializeByString($value);
-                }
+            return self::createResponseFromQuery($string);
+        }
+    }
+
+    /**
+     * Create response instance by JSON object
+     * @param stdClass $Value response object
+     * @return Token|Error response instance: error or token data
+     */
+    private static function createResponseFromJson($Value) {
+        switch (true) {
+            case isset($Value->error):
+                return Error::initializeByObject($Value);
+            default:
+                return Token::initializeByObject($Value);
+        }
+    }
+
+    /**
+     * Create response instance by query string
+     * @param string $queryString query string
+     * @return Code|Error response instance: error or code data
+     * @throws RuntimeException when response has a suspect string
+     */
+    private static function createResponseFromQuery($queryString) {
+        $value = parse_url($queryString, PHP_URL_QUERY);
+        if ($value !== false) {
+            switch (true) {
+                case strpos($value, 'error=') === 0:
+                    return Error::initializeByString($value);
+                case strpos($value, 'code=') === 0:
+                    return Code::initializeByString($value);
             }
         }
-        throw new RuntimeException(sprintf('suspect data returned: %s', $string));
+        throw new RuntimeException(sprintf('suspect data returned: %s', $queryString));
     }
 }
